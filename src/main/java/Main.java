77ea7c57d7605f8582615add2ef5ef386cf0b390
Reneddy <beturen@gmail.com>
2022-11-26 02:42:38 +0300

    Created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import ru.netology.sqr.SQRService;
 public class Main {
     public static void main(String[] args) {
         SQRService service = new SQRService();
-        int sqrNumber = service.calcSQRT(0,0);
+        int sqrNumber = service.calcSQRT(200,300);
         System.out.print(sqrNumber);
     }
 }
